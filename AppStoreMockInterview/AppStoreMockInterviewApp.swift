1d53086fea8f709f95f5cc757a934e9e169cb204
RajeshTriadiNoftarizal <rajeshtriadi14@gmail.com>
2024-08-05 22:29:16 +0700

    Initial Commit

---
+++ AppStoreMockInterview/AppStoreMockInterviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreMockInterviewApp.swift
+//  AppStoreMockInterview
+//
+//  Created by Rajesh Triadi Noftarizal on 05/08/24.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreMockInterviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
